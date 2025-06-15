abf890dbd95f40bb228e2414f0d7bb9be07522c1
Shota Watanabe <167762459+shota548@users.noreply.github.com>
2025-06-15 19:28:06 +0900

    0615

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,21 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: showatan <showatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/15 12:16:25 by marvin            #+#    #+#             */
-/*   Updated: 2025/06/15 12:46:19 by marvin           ###   ########.fr       */
+/*   Created: 2025/06/15 19:20:11 by showatan          #+#    #+#             */
+/*   Updated: 2025/06/15 19:20:11 by showatan         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char    *get_next_line(int fd)
+{
+
+}
 
 find_newline(int fd, char **line, char **st_arr, char *buf) 
 {
@@ -16,6 +23,11 @@ find_newline(int fd, char **line, char **st_arr, char *buf)
 }
 
 read_fd(int fd, char **line, char **st_arr)
+{
+    
+}
+
+int    free_all(char **line, char **st_arr, int fd)
 {
     
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 12:42:53 by marvin            #+#    #+#             */
-/*   Updated: 2025/06/15 12:45:42 by marvin           ###   ########.fr       */
+/*   Updated: 2025/06/15 13:59:18 by marvin           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -17,5 +17,13 @@
 # include <limits.h>
 # include <stddef.h>
 # include <unistd.h>
+
+find_newline(int fd, char **line, char **st_arr, char *buf);
+read_fd(int fd, char **line, char **st_arr);
+void	ft_bzero(void *b, size_t len);
+void *ft_calloc(size_t count, size_t size);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int search_c );
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
